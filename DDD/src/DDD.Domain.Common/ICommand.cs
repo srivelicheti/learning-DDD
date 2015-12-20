3e8e139cba3bc9cc88bb20eba1e19fbda10064a0
Sri Velicheti <velicheti.harsha@gmail.com>
2015-12-20 14:09:03 -0500

    Initial DDD setup

---
+++ DDD/src/DDD.Domain.Common/ICommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.Common
+{
+    public interface ICommand
+    {
+        Guid ID { get; }
+    }
+    public abstract class Command : ICommand
+    {
+        public Guid ID { get; private set; }
+
+        public Command(Guid id)
+        {
+            ID = id;
+        }
+        public Command() : this(Guid.NewGuid())
+        { }
+    }
+}
+
+
