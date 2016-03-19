ee362d31b7236ed96f1c71044bc531b161e69e44
srivelicheti <velicheti.harsha@gmail.com>
2016-03-19 19:32:38 -0400

    resolved issue with post commit events

--- DDD/src/DDD.Domain.Common/Event/EventBus.cs
+++ DDD/src/DDD.Domain.Common/Event/EventBus.cs
@@ -11,13 +11,14 @@ namespace DDD.Domain.Common.Event
     public class DomainEventBus
     {
         public static IList<IEventHandler> EventHandlers { get; } = new List<IEventHandler>();
-        private readonly ConcurrentQueue<DomainEvent> _postCommitEvents = new ConcurrentQueue<DomainEvent>();
+        private readonly ConcurrentDictionary<DomainEvent,IEventHandler> _postCommitEvents = new ConcurrentDictionary<DomainEvent,IEventHandler>();
         public static void Subscribe<TEvent>(IEventHandler<TEvent> eventHandler) where TEvent : DomainEvent
         {
             EventHandlers.Add(eventHandler);
         }
         public void Publish<TEvent>(TEvent e) where TEvent : DomainEvent
         {
+            
             foreach (var handler in EventHandlers)
             {
                 var eventHandler = handler as IEventHandler<TEvent>;
@@ -43,14 +44,41 @@ namespace DDD.Domain.Common.Event
 
         public void QueueForPostCommit<TEvent>(TEvent e) where TEvent : DomainEvent
         {
-            _postCommitEvents.Enqueue(e);
+            foreach (var handler in EventHandlers)
+            {
+                var eventHandler = handler as IEventHandler<TEvent>;
+                if (eventHandler != null)
+                {
+                    try
+                    {
+                        _postCommitEvents.TryAdd(e, eventHandler);
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine("event handling error: " + ex.Message);
+                        throw;
+                    }
+                }
+                
+            }
         }
 
         public void PublishQueuedPostCommitEvents()
         {
             foreach (var e in _postCommitEvents)
             {
-                Publish(e);
+                try {
+                    //dynamic handler = e.Value;
+                    var handlerType = e.Value.GetType();
+                    var method = handlerType.GetMethod("Handle");
+                    method.Invoke(e.Value, new object[] { e.Key });
+                    //handler.Handle(  );
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("event handling error: " + ex.Message);
+                    throw;
+                }                
             }
         }
     }
