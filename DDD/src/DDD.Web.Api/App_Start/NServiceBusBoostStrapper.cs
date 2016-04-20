5f52d82f53d8e77d137271e7a3626cbf32ad7989
Sri Velicheti <velicheti.harsha@gmail.com>
2016-04-20 18:19:46 -0400

    Fix email serv issue

--- DDD/src/DDD.Web.Api/App_Start/NServiceBusBoostStrapper.cs
+++ DDD/src/DDD.Web.Api/App_Start/NServiceBusBoostStrapper.cs
@@ -205,4 +205,21 @@ namespace DDD.Web.Api.App_Start
             
         }
     }
+
+    class AuthorizeSubscriptions : IAuthorizeSubscriptions
+    {
+
+        public bool AuthorizeSubscribe(string messageType, string clientEndpoint, IDictionary<string, string> headers)
+        {
+            return true;
+            //string lowerEndpointName = clientEndpoint.ToLowerInvariant();
+            //return lowerEndpointName.StartsWith("samples.pubsub.subscriber1") ||
+            //       lowerEndpointName.StartsWith("samples.pubsub.subscriber2");
+        }
+
+        public bool AuthorizeUnsubscribe(string messageType, string clientEndpoint, IDictionary<string, string> headers)
+        {
+            return true;
+        }
+    }
 }
