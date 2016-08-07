4788f220291b647b4330f9855db1c53411a7c43b
srivelicheti <velicheti.harsha@gmail.com>
2016-08-07 14:29:36 -0400

    Add Site Functionality

---
+++ DDD/src/ddd.web.ui/wwwroot/js/models/AddSiteViewModel.js
@@ -0,0 +1,47 @@
+﻿define(["knockout", "lodash","models/AddressViewModel","models/ContactDetailViewModel" ,"models/BaseViewModel"], function (ko, _,AddressViewModel,ContactDetailViewModel ,BaseViewModel) {
+    function SiteHoliday(holidayName, holidayDate) {
+        this.holidayName = ko.observable(holidayName);
+        this.holidayDate = ko.observable(holidayDate);
+    }
+
+    function SiteRate(minAge, maxAge, rate) {
+        this.minAge = ko.observable(minAge);
+        this.maxAge = ko.observable(maxAge);
+        this.rate = ko.observable(rate);
+    }
+
+    function SiteDetails(siteNumber,siteName,contractorEin,contractStartDate,contractEndDate,siteType,siteFacitlityType,addressDetails,phoneNumber,contactDetails) {
+        this.SiteNumber =ko.observable(siteNumber);
+        this.SiteName = ko.observable(siteName);
+        this.ContractorEin = ko.observable(contractorEin);
+        this.ContractStartDate = ko.observable(contractStartDate);
+        this.ContractEndDate = ko.observable(contractEndDate);
+        this.SiteType = ko.observable(siteType);
+        this.SiteFacilityType = ko.observable(siteFacitlityType);
+        this.Address = addressDetails;
+        this.PhoneNumber = ko.observable(phoneNumber);
+        this.ContactDetail = contactDetails;
+    }
+
+    return {
+        SiteDetails: new SiteDetails("",
+            "",
+            '',
+            '',
+            '',
+            '',
+            '',
+            new AddressViewModel(),
+            '',
+            new ContactDetailViewModel()),
+        SiteHolidays: ko.observableArray(),
+        SiteRates: ko.observableArray(),
+        AddSiteHoliday: function(holidayName, holidayDate) {
+            this.SiteHolidays().push(new SiteHoliday(holidayName, holidayDate));
+        },
+        AddSiteRate: function(minAge, maxAge, rate) {
+            this.SiteRates().push(new SiteRate(minAge, maxAge, rate));
+        }
+    };
+
+});
---
+++ DDD/src/ddd.web.ui/wwwroot/js/models/ContactDetailViewModel.js
@@ -0,0 +1,16 @@
+﻿define(["knockout", "lodash", "models/BaseViewModel"], function (ko, _, BaseViewModel) {
+
+    var vm = function (lastName,firstName,phoneNumber,email) {
+        this.LastName = lastName;
+        this.FirstName = firstName;
+        this.PhoneNumber = phoneNumber;
+        this.Email = email;
+        
+    };
+
+    _.extend(vm.prototype, BaseViewModel.prototype,
+    {
+        initialize: function () { }
+    });
+    return vm;
+});
